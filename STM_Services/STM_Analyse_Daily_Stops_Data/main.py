c7b41d6108625ffd42cb21b948113d17d6c26e00
abouillon-ets <alexandre.bouillon.1@ens.etsmtl.ca>
2023-11-20 23:47:36 -0500

    update 15% des stop_times sans valeur

--- STM_Services/STM_Analyse_Daily_Stops_Data/main.py
+++ STM_Services/STM_Analyse_Daily_Stops_Data/main.py
@@ -16,21 +16,26 @@ def lambda_handler(event, context):
     event_date = event['date']  # e.g., '20231113'
     timezone = event['timezone']  # e.g., 'Canada/Eastern'
 
+    eastern_tz = pytz.timezone('Canada/Eastern')
+
+    TIME_THRESHOLD = 18000  # 5 hours in seconds
+
     # We get the date to evaluate and also determine the next day for the GTFS Json files
     event_datetime = datetime.strptime(event_date, '%Y%m%d')
     next_day = event_datetime + timedelta(days=1)
 
-    # Determine the folders name in format # e.g. '2023-11-09'
+    # Calculate UNIX timestamp for 7:20 AM on the next day to use a Threshold when fetching from S3 bucket
+    seven_twenty_am_unix = time_to_unix("07:20:00", next_day, timezone)
+    
+    # Determine the folders name in format # e.g. '2023-11-09' & '2023-11-10'
     event_date_folder = event_datetime.strftime('%Y-%m-%d')
     next_day_folder = next_day.strftime('%Y-%m-%d')
 
     # Create the dictionary for the GTFS Live data
     json_dict = {}
-    json_dict.update(process_json_files_s3(json_bucket_name, f'{event_date_folder}/'))
-    json_dict.update(process_json_files_s3(json_bucket_name, f'{next_day_folder}/'))
 
     event_date_csv = event_datetime.strftime('%Y-%m-%d')
-    csv_file_name = f'{event_date_csv}/filtered_stop_times.csv'
+    csv_file_name = f'{event_date_csv}/filtered_stop_times/filtered_stop_times.csv'
     csv_obj = s3.get_object(Bucket=csv_bucket_name, Key=csv_file_name)
     csv_data = pd.read_csv(csv_obj['Body'])
 
@@ -39,22 +44,26 @@ def lambda_handler(event, context):
 
     # Prepare a new column for the offset
     csv_data['offset'] = None
+    csv_data['Stop_Sequence_Found'] = None
 
-    # Iterate through the rows of the CSV data
-    for index, row in csv_data.iterrows():
-        trip_id = str(row['trip_id'])
-        stop_sequence = row['stop_sequence']
-        if trip_id in json_dict:
-            for item in json_dict[trip_id]:
-                if item['vehicle']['currentStopSequence'] == stop_sequence:
-                    arrival_time_unix = row['arrival_time_unix'] #CSV_file GTFS STATIC
-                    timestamp_unix = int(item['vehicle']['timestamp']) #GTFS VehiclePositions
-                    # Calculate the offset
-                    offset = timestamp_unix - arrival_time_unix
-                    # Update the offset column
-                    csv_data.at[index, 'offset'] = offset
-                    break  # Break as soon as a match is found to avoid overwriting with subsequent JSON files
+    # To store the last stop sequence for each trip
+    last_stop_sequences = csv_data.groupby('trip_id')['stop_sequence'].max().to_dict()
 
+    # To keep track of the previous trip and stop sequence for each vehicle
+    previous_trip_stop = {}
+
+    # Process data from event_date_folder and calculate offsets
+    #json_dict = process_json_files_s3(json_bucket_name, f'{event_date_folder}/', event_datetime, eastern_tz)
+    #process_offsets(csv_data, json_dict, previous_trip_stop, last_stop_sequences, TIME_THRESHOLD)
+
+    # Process data from next_day_folder, skipping already processed trip_ids
+    json_dict_next_day = process_json_files_s3(json_bucket_name, f'{next_day_folder}/', event_datetime, eastern_tz, seven_twenty_am_unix)
+    process_offsets(csv_data, json_dict_next_day, previous_trip_stop, last_stop_sequences, TIME_THRESHOLD, skip_processed=True)
+    
+    # Print the number of items (keys) in json_dict
+    print(f"Number of items in json_dict: {len(json_dict)}")
+    print(f"Number of items in json_dict: {len(json_dict_next_day)}")
+    
     # Save CSV back to S3
     csv_buffer = StringIO()
     csv_data.to_csv(csv_buffer, index=False)
@@ -62,30 +71,115 @@ def lambda_handler(event, context):
                                                                                     f'/updated_filtered_stop_times.csv')
 
 
-def process_json_files_s3(bucket_name, prefix):
+def process_json_files_s3(bucket_name, prefix, event_datetime, timezone, cutoff_timestamp=None):
     json_dict = {}
-    response = s3.list_objects_v2(Bucket=bucket_name, Prefix=prefix)
-    for item in response.get('Contents', []):
-        key = item['Key']
-        if key.endswith('.json.gz'):
-            json_obj = s3.get_object(Bucket=bucket_name, Key=key)
-            with gzip.GzipFile(fileobj=json_obj['Body']) as gzipfile:
-                json_data = json.load(gzipfile)
-                # Access the 'entity' field from json_data
-                entities = json_data.get('entity', [])
-                for entity_item in entities:
-                    trip_id = entity_item['vehicle']['trip']['tripId']
-                    if trip_id not in json_dict:
-                        json_dict[trip_id] = []
-                    json_dict[trip_id].append(entity_item)
+    file_count = 0
+    continuation_token = None
+    next_day = event_datetime + timedelta(days=1)
+
+    while True:
+        # List objects with pagination
+        if continuation_token:
+            response = s3.list_objects_v2(Bucket=bucket_name, Prefix=prefix, ContinuationToken=continuation_token)
+        else:
+            response = s3.list_objects_v2(Bucket=bucket_name, Prefix=prefix)
+
+        for item in response.get('Contents', []):
+            key = item['Key']
+            if key.endswith('.json.gz'):
+                # Extract timestamp from file name
+                file_timestamp = int(key.split('_')[-1].split('.')[0])
+                
+                # Check if file_timestamp is for the next day
+                file_datetime = datetime.fromtimestamp(file_timestamp, timezone)
+                file_date = file_datetime.date()
+                
+                # Process the file only if its timestamp is less than or equal to the cutoff timestamp
+                if cutoff_timestamp is None or file_timestamp <= cutoff_timestamp:
+                    file_count += 1
+                    json_obj = s3.get_object(Bucket=bucket_name, Key=key)
+                    with gzip.GzipFile(fileobj=json_obj['Body']) as gzipfile:
+                        json_data = json.load(gzipfile)
+                        entities = json_data.get('entity', [])
+                        for entity_item in entities:
+                            trip_id = entity_item['vehicle']['trip']['tripId']
+                            # Skip if trip_id is already in json_dict and file_date is the next day
+                            if trip_id in json_dict and file_date == next_day.date():
+                                continue
+                            if trip_id not in json_dict:
+                                json_dict[trip_id] = []
+                            json_dict[trip_id].append(entity_item)
+
+        # Check if there are more files to fetch
+        if response.get('IsTruncated'):
+            continuation_token = response.get('NextContinuationToken')
+        else:
+            break
+
+    print(f"Number of '.json.gz' files in bucket '{bucket_name}' with prefix '{prefix}': {file_count}")
     return json_dict
 
 
-# Function to convert UNIX timestamp to seconds of the day
-def unix_to_seconds(unix_timestamp, timezone):
-    dt = datetime.fromtimestamp(unix_timestamp, tz=pytz.timezone(timezone))
-    seconds = (dt - dt.replace(hour=0, minute=0, second=0, microsecond=0)).total_seconds()
-    return seconds
+
+# Iterate through the rows of the CSV data
+def process_offsets(csv_data, json_dict, previous_trip_stop, last_stop_sequences, TIME_THRESHOLD, skip_processed=False):
+    for index, row in csv_data.iterrows():
+        trip_id = str(row['trip_id'])
+        stop_sequence = row['stop_sequence']
+        if trip_id in json_dict:
+
+            # Skip processing this trip_id if 'offset' already exists and skip_processed is True
+            if skip_processed and csv_data.at[index, 'offset'] is not None:
+                continue
+
+            for item in json_dict[trip_id]:
+                # Data from GTFS
+                vehicle_id = item['vehicle']['vehicle']['id']
+                vehicle_trip_id = item['vehicle']['trip']['tripId']
+                vehicle_stop_sequence = item['vehicle']['currentStopSequence']
+                vehicle_status = item['vehicle']['currentStatus']
+
+                # Check for transition to a new trip
+                if vehicle_id in previous_trip_stop:
+                    previous_trip = previous_trip_stop[vehicle_id]
+                    if 'trip_id' in previous_trip and previous_trip['trip_id'] != vehicle_trip_id and vehicle_stop_sequence in [1, 2]:
+                        # Handle the last stop of the previous trip
+                        prev_trip_id = previous_trip['trip_id']
+                        if str(prev_trip_id) in last_stop_sequences:
+                            last_stop_seq = last_stop_sequences[str(prev_trip_id)]
+                            last_stop_data = csv_data[(csv_data['trip_id'] == str(prev_trip_id)) & (csv_data['stop_sequence'] == last_stop_seq)]
+                            if not last_stop_data.empty:
+                                last_stop_row = last_stop_data.iloc[0]
+                                try:
+                                    last_stop_arrival_time_unix = int(last_stop_row['arrival_time_unix'])
+                                    timestamp_unix = int(item['vehicle']['timestamp'])
+                                    offset = timestamp_unix - last_stop_arrival_time_unix
+                                    if not isinstance(offset, int):
+                                        raise TypeError(f"Offset is not an integer. Offset: {offset}, Type: {type(offset)}")
+                                    if abs(offset) <= TIME_THRESHOLD:
+                                        last_stop_index = last_stop_data.index[0]
+                                        csv_data.at[last_stop_index, 'offset'] = offset
+                                except (TypeError, ValueError) as e:
+                                    print(f"Error in offset calculation for previous trip: {e}")
+                                    print(f"prev_trip_id: {prev_trip_id}, timestamp_unix: {timestamp_unix}, last_stop_arrival_time_unix: {last_stop_arrival_time_unix}")
+
+                # Normal processing for current trip 
+                if vehicle_stop_sequence >= stop_sequence and vehicle_status in ["IN_TRANSIT_TO", "STOPPED_AT"]:
+                    try:
+                        arrival_time_unix = int(row['arrival_time_unix'])  # Ensure integer
+                        timestamp_unix = int(item['vehicle']['timestamp'])  # Ensure integer
+                        offset = timestamp_unix - arrival_time_unix
+                        
+                        if not isinstance(offset, int):
+                            raise TypeError(f"Offset is not an integer. Offset: {offset}, Type: {type(offset)}")
+
+                        if abs(offset) <= TIME_THRESHOLD:
+                            csv_data.at[index, 'offset'] = offset
+                            break  # Break as soon as a match is found
+
+                    except (TypeError, ValueError) as e:
+                        print(f"Error in offset calculation: {e}")
+                        print(f"trip_id: {trip_id}, timestamp_unix: {timestamp_unix}, arrival_time_unix: {arrival_time_unix}")
 
 
 def time_to_unix(time_str, base_date, timezone_str):
