f74acf45577f931294edbf33e4823d17fde46880
Victor St-Louis <victor.st-louis@hotmail.ca>
2024-03-12 21:36:51 -0400

    Add Mongo in BIXI_Fetch_GBFS_Station_Status

--- BIXI_Services/BIXI_Fetch_GBFS_Station_Status/main.py
+++ BIXI_Services/BIXI_Fetch_GBFS_Station_Status/main.py
@@ -5,11 +5,14 @@ import pandas as pd
 import pytz
 from datetime import datetime
 from urllib.request import Request, urlopen
-
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
 
 def lambda_handler(event, context):
     s3 = boto3.client('s3')
-
+    atlas_uri = os.environ.get('ATLAS_URI')
+    db_name = os.environ.get('MONGO_DATABASE_NAME')
+    collection_name = event['collection_name']
     bucket_name = event['bucket_name']
     temp_file_path = event.get('file_path', '/tmp/file.parquet')
 
@@ -31,7 +34,8 @@ def lambda_handler(event, context):
     # Deserialize JSON data
     data_dict=json.loads(data)
     #Create dataframe with only the stations status
-    df = pd.DataFrame(data_dict["data"]["stations"])
+    stations = data_dict["data"]["stations"]
+    df = pd.DataFrame(stations)
 
     # Save Dataframe 
     df.to_parquet(temp_file_path, compression="gzip")
@@ -52,6 +56,18 @@ def lambda_handler(event, context):
         except Exception as e:
             print(f"Failed to delete temporary file: {e}")
     
+
+    # Write to Mongo
+    mongoClient = MongoClient(atlas_uri, server_api=ServerApi('1'), tls=True, tlsAllowInvalidCertificates=True)
+    document_name = f'gbfs_data_station_status_{fetch_time_unix}'
+    try:
+        document = { "document_name": document_name, "stations": stations }
+        db = mongoClient[db_name]
+        collection = db[collection_name]
+        inserted = collection.insert_one(document)
+        print(f"inserted document with id: {inserted.inserted_id}")
+    except Exception as e:
+        print(f"Mongo insert returned an error: {e}")
 
 # You can set the environment variables 'API_URL' and 'bucket_name' accordingly.
 # Example usage: lambda_handler({'bucket_name': 'my-s3-bucket'}, None)
