f982e4a50bfd990d9f1f2b0d2c1c34d3ae0fa7a6
K. Allagbe <kotchikpa.allagbe@gmail.com>
2024-03-31 22:15:50 -0400

    #90: extract transform load + tests

--- BIXI_Services/BIXI_Historical_Data_Processor/main.py
+++ BIXI_Services/BIXI_Historical_Data_Processor/main.py
@@ -1,2 +1,214 @@
+import asyncio
+import os
+import traceback
+import zipfile
+from io import BytesIO
+
+import pandas as pd
+import requests
+from motor.motor_asyncio import AsyncIOMotorClient
+from pymongo import InsertOne, UpdateOne
+
+REQUIRED_COLUMNS = [
+    "STARTSTATIONNAME",
+    "STARTSTATIONARRONDISSEMENT",
+    "STARTSTATIONLATITUDE",
+    "STARTSTATIONLONGITUDE",
+    "ENDSTATIONNAME",
+    "ENDSTATIONARRONDISSEMENT",
+    "ENDSTATIONLATITUDE",
+    "ENDSTATIONLONGITUDE",
+    "STARTTIMEMS",
+    "ENDTIMEMS",
+]
+
+
+async def save_url(collection, url, year=None):
+    operation = InsertOne({"_id": url, "year": year})
+    await collection.bulk_write([operation], ordered=False)
+    print(f"saved: {url}")
+
+
+def prepare_location_data(chunk):
+    return (
+        pd.concat(
+            [
+                chunk[
+                    [
+                        "STARTSTATIONNAME",
+                        "STARTSTATIONARRONDISSEMENT",
+                        "STARTSTATIONLATITUDE",
+                        "STARTSTATIONLONGITUDE",
+                    ]
+                ].rename(
+                    columns={
+                        "STARTSTATIONNAME": "name",
+                        "STARTSTATIONARRONDISSEMENT": "arrondissement",
+                        "STARTSTATIONLATITUDE": "latitude",
+                        "STARTSTATIONLONGITUDE": "longitude",
+                    }
+                ),
+                chunk[
+                    [
+                        "ENDSTATIONNAME",
+                        "ENDSTATIONARRONDISSEMENT",
+                        "ENDSTATIONLATITUDE",
+                        "ENDSTATIONLONGITUDE",
+                    ]
+                ].rename(
+                    columns={
+                        "ENDSTATIONNAME": "name",
+                        "ENDSTATIONARRONDISSEMENT": "arrondissement",
+                        "ENDSTATIONLATITUDE": "latitude",
+                        "ENDSTATIONLONGITUDE": "longitude",
+                    }
+                ),
+            ]
+        )
+        .drop_duplicates(subset=["name"])
+        .reset_index(drop=True)
+    )
+
+
+def create_update_operations(locations):
+    operations = [
+        UpdateOne(
+            {"_id": row["name"]},
+            {
+                "$setOnInsert": {
+                    "arrondissement": row["arrondissement"],
+                    "latitude": row["latitude"],
+                    "longitude": row["longitude"],
+                }
+            },
+            upsert=True,
+        )
+        for _, row in locations.iterrows()
+    ]
+    return operations
+
+
+async def process_locations(chunk, collection):
+    locations = prepare_location_data(chunk)
+    if operations := create_update_operations(locations):
+        await collection.bulk_write(operations, ordered=False)
+
+
+async def process_trips(chunk, collection):
+    chunk["DURATION"] = chunk["ENDTIMEMS"] - chunk["STARTTIMEMS"]
+    trip_docs = chunk[
+        ["STARTSTATIONNAME", "ENDSTATIONNAME", "STARTTIMEMS", "ENDTIMEMS", "DURATION"]
+    ].to_dict("records")
+    if trip_docs:
+        operations = [InsertOne(doc) for doc in trip_docs]
+        await collection.bulk_write(operations, ordered=False)
+
+
+async def worker(queue: asyncio.Queue):
+    while True:
+        task = await queue.get()
+        try:
+            await task
+        finally:
+            queue.task_done()
+
+
+async def get_highest_starttimems(col):
+    doc = await col.find_one(sort=[("STARTTIMEMS", -1)], projection={"STARTTIMEMS": 1})
+    return doc["STARTTIMEMS"] if doc else None
+
+
+def extract(url, path):
+    print("start download and extract", url)
+    with requests.get(url) as r:
+        r.raise_for_status()
+        with zipfile.ZipFile(BytesIO(r.content)) as z:
+            os.makedirs(path, exist_ok=True)
+            z.extractall(path=path)
+            extracted = z.namelist()
+            print("extracted files:", extracted)
+            print("download and extract completed successfully.")
+    if not extracted:
+        raise Exception("No file extracted.")
+    return [os.path.join(path, file) for file in extracted if file.endswith(".csv")]
+
+
+async def transform_load(
+    csv_files,
+    col_locations,
+    col_trips,
+    q_size,
+    chunk_size,
+    concurrency,
+):
+    max_starttimems = await get_highest_starttimems(col_trips)
+    q = asyncio.Queue(q_size)
+    workers = [asyncio.create_task(worker(q)) for _ in range(concurrency)]
+    for file in csv_files:
+        # ensure file exists
+        if not os.path.exists(file):
+            print(file, "doesn't exist.")
+            continue
+        print("Starting processing:", file)
+        # ensure the csv has the right columns
+        df = pd.read_csv(file, nrows=0)
+        if not all(column in df.columns for column in REQUIRED_COLUMNS):
+            print(f"CSV file {file} does not contain required columns.")
+            continue
+        # enqueue tasks for processing each chunk
+        for chunk in pd.read_csv(file, chunksize=chunk_size):
+            # ensure we don't save data we already have
+            if max_starttimems is not None:
+                chunk = chunk[chunk["STARTTIMEMS"] > max_starttimems]
+            await q.put(asyncio.create_task(process_locations(chunk, col_locations)))
+            await q.put(asyncio.create_task(process_trips(chunk, col_trips)))
+    await q.join()
+    for w in workers:
+        w.cancel()
+    await asyncio.gather(*workers, return_exceptions=True)
+
+
+async def etl(url_item):
+    print("ETL process started for URL:", url_item[0])
+    url, year = url_item
+    # db object
+    client = AsyncIOMotorClient(os.environ["ATLAS_URI"])
+    db = client[os.environ["MONGO_DATABASE_NAME"]]
+    # extract
+    files = extract(url, os.environ["BIXI_DEFAULT_EXTRACT_PATH"])
+    # transform and load
+    await transform_load(
+        files,
+        db[os.environ["BIXI_LOCATION_COLLECTION"]],
+        db[os.environ["BIXI_TRIP_COLLECTION"]],
+        int(os.environ["BIXI_QUEUE_SIZE"]),
+        int(os.environ["BIXI_CHUNK_SIZE"]),
+        int(os.environ["BIXI_CONCURRENCY"]),
+    )
+    # cleaning up
+    await save_url(db[os.environ["BIXI_URL_COLLECTION"]], url, year)
+    for file_path in files:
+        os.remove(file_path)
+    return file_path
+
+
+async def main(event, context):
+    print("historic data processing started.")
+    try:
+        urls: dict[int, str] = event["urls"]
+        if not urls:
+            print("no new data to process.")
+            return {"status": "Success", "filenames": None}
+        sorted_urls = dict(sorted(urls.items()))
+        files = []
+        for year, url in sorted_urls.items():
+            files.extend(await etl((url, year)))
+        print("historic data processed successfully.")
+        return {"status": "Success", "filenames": files}
+    except Exception as e:
+        traceback.print_exc()
+        return {"status": "Error", "error": str(e), "processedUrls": []}
+
+
 def handler(event, context):
-    pass
+    return asyncio.run(main(event, context))
---
+++ tests/integration/test_bixi_historical_data_processor_integration.py
@@ -0,0 +1,136 @@
+import os
+from unittest.async_case import IsolatedAsyncioTestCase
+from unittest.mock import patch
+
+import pandas as pd
+from motor.motor_asyncio import AsyncIOMotorClient
+
+from BIXI_Services.BIXI_Historical_Data_Processor.main import (
+    REQUIRED_COLUMNS,
+    transform_load,
+)
+
+@patch.dict(os.environ, {"MONGO_DATABASE_NAME": "test-monitoring-mtl"}, clear=False)
+class TestTransformLoadIntegration(IsolatedAsyncioTestCase):
+    """Testing the transform_load function's integration with the database. Depends on 
+    test csv files in ./test_files."""
+
+    async def asyncSetUp(self):
+        assert os.environ["MONGO_DATABASE_NAME"] == "test-monitoring-mtl"
+        self.uri = os.environ["ATLAS_URI"]
+        self.db_name = os.environ["MONGO_DATABASE_NAME"]
+        self.location_collection_name = "location_collection"
+        self.trip_collection_name = "trip_collection"
+        self.client = AsyncIOMotorClient(self.uri)
+        self.db = self.client[self.db_name]
+        self.base_path = os.path.abspath(os.path.dirname(__file__))
+        self.test_files = [
+            os.path.join(self.base_path, "test_files", "file1.csv"),
+            os.path.join(self.base_path, "test_files", "file2.csv"),
+        ]
+        for file in self.test_files:
+            assert os.path.exists(file), f"{file} does not exist"
+        self.queue_size = 1
+        self.chunk_size = 250000
+        self.concurrency = 4
+
+    async def asyncTearDown(self):
+        await self.db[self.location_collection_name].drop()
+        await self.db[self.trip_collection_name].drop()
+        self.client.close()
+
+    async def test_transform_load(self):
+        await transform_load(
+            self.test_files,
+            self.db[self.location_collection_name],
+            self.db[self.trip_collection_name],
+            self.queue_size,
+            self.chunk_size,
+            self.concurrency,
+        )
+        trip_count = await self.db[self.trip_collection_name].count_documents({})
+        location_count = await self.db[self.location_collection_name].count_documents(
+            {}
+        )
+        self.assertEqual(trip_count, 40)
+        self.assertEqual(location_count, 56)
+
+    async def test_transform_load_with_non_existing_files(self):
+        non_existing_files = [
+            os.path.join(self.base_path, "test_files", "non_existing_file1.csv"),
+            os.path.join(self.base_path, "test_files", "non_existing_file2.csv"),
+        ]
+        await transform_load(
+            non_existing_files,
+            self.db[self.location_collection_name],
+            self.db[self.trip_collection_name],
+            self.queue_size,
+            self.chunk_size,
+            self.concurrency,
+        )
+        trip_count = await self.db[self.trip_collection_name].count_documents({})
+        location_count = await self.db[self.location_collection_name].count_documents(
+            {}
+        )
+        self.assertEqual(trip_count, 0)
+        self.assertEqual(location_count, 0)
+
+    async def test_transform_load_with_missing_columns(self):
+        original_file = os.path.join(self.base_path, "test_files", "file1.csv")
+        modified_file = os.path.join(self.base_path, "test_files", "modified_file.csv")
+        # read original and remove a column
+        df = pd.read_csv(original_file)
+        df.drop(columns=[REQUIRED_COLUMNS[0]], inplace=True)
+        df.to_csv(modified_file, index=False)
+        try:
+            await transform_load(
+                [modified_file],
+                self.db[self.location_collection_name],
+                self.db[self.trip_collection_name],
+                self.queue_size,
+                self.chunk_size,
+                self.concurrency,
+            )
+            trip_count = await self.db[self.trip_collection_name].count_documents({})
+            location_count = await self.db[
+                self.location_collection_name
+            ].count_documents({})
+            self.assertEqual(trip_count, 0)
+            self.assertEqual(location_count, 0)
+        finally:
+            os.remove(modified_file)
+
+    async def test_transform_load_with_data_duplication(self):
+        await transform_load(
+            [os.path.join(self.base_path, "test_files", "file1.csv")],
+            self.db[self.location_collection_name],
+            self.db[self.trip_collection_name],
+            self.queue_size,
+            self.chunk_size,
+            self.concurrency,
+        )
+
+        # create a file with old + new data
+        combined_file = os.path.join(
+            self.base_path, "test_files", "file1_file2_combined.csv"
+        )
+        df1 = pd.read_csv(os.path.join(self.base_path, "test_files", "file1.csv"))
+        df2 = pd.read_csv(os.path.join(self.base_path, "test_files", "file2.csv"))
+        combined_df = pd.concat([df1, df2])
+        assert len(combined_df) == 40, "Combined file does not contain 40 rows"
+        combined_df.to_csv(combined_file, index=False)
+
+        # should save only the new data
+        try:
+            await transform_load(
+                [combined_file],
+                self.db[self.location_collection_name],
+                self.db[self.trip_collection_name],
+                self.queue_size,
+                self.chunk_size,
+                self.concurrency,
+            )
+            trip_count = await self.db[self.trip_collection_name].count_documents({})
+            self.assertLessEqual(trip_count, 40)
+        finally:
+            os.remove(combined_file)
